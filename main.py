205296e0195e7d1964f2af38f108153227d96d89
ethan <ethanmacdonald04@gmail.com>
2016-10-02 11:59:42 -0300

    made a bunch of enhancements and made way more efficient!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from lib_terminal import *
 import time
   
 current_array = []
+prev_lines = []
     
 def main():
     global current_array
@@ -12,7 +13,6 @@ def main():
     column = len(current_line)
     term_height = Terminal.Position.get_height()-3
     prev_lines = []
-    last_line_len = 0
     
     look = False
     look_array = []
@@ -30,61 +30,62 @@ def main():
         
         column = len("".join(split_array))
         
-        if (current_pressed == "\x08"):
+        if (current_pressed == "\x13"):
+            current_array.pop()
+            current_array.append("EMPTY!")
+        
+        elif (current_pressed == "\x08"):
             for i in range(0, 2):
                 try:
                     current_array.pop()
                 except:
                     main()
-            #amount_items -= 1
             column -= 2
             current_line = "".join(current_array)
+            
+            if (current_line == ""):
+                list_temp = prev_lines.pop()
+                list_temp = list(list_temp)
+                for i in range(0, len("".join(list_temp))):
+                    current_array.insert(i, list_temp.pop(0))
             
         elif (current_pressed == "\r"):
             current_array.pop()
             term_height -= 1
             line += 1
-            #current_array = []
             current_array.append("\n")
+            prev_lines.append("".join(current_array).rstrip("\r"))
+            current_array = []
         
         elif (current_pressed == ""):
             break
         
         else:
-            message = "Accepted Key '{}'".format(current_pressed)
-            
+            message = "Accepted Key '{}'".format(current_pressed)  
             
         look_join = "".join(look_array)    
         if (current_pressed == "$"):
             look_array = []
             look = True
-            
-        
                 
         if (look == True and current_pressed == "$" and look_join != ""):
             look = False
-            message = "look = False"
         
-        #message = current_array
+        message = prev_lines, current_array
         Terminal.clear()
-        Terminal.Color.light_grey("{}".format(current_line))
-        Terminal.Color.purple("column:{}, line:{}, last:{}; {}".format(column, line, last_line_len, message), 0, term_height)
-          
-        prev_lines = "".join(current_array).split("\n")
-        last_line = prev_lines.pop(line-2)
-        last_line_len = len(str(last_line))
+        Terminal.Color.light_grey("{}{}".format("".join(prev_lines), current_line))
+        Terminal.Color.green("column:{}, line:{}; {}".format(column, line, message), 0, term_height)
+        
         current_pressed = getch()
-        time.sleep(0.03)
+        time.sleep(0.05)
         if (look == True):
             look_array.append(current_pressed)
-            message = look_join
             
             if ("quit" in look_join):
                 break
             elif ("clean" in look_join):
                 current_array = []
                 look = False
-                message = "look = False"
               
     Terminal.clear()
     quit()
